fad9625a6f41b9d4fe9dbcb170a63e46e4f6a77e
Tiago de Castro Lima <casmei@protonmail.com>
2022-06-16 13:03:42 -0300

    feat: Listagem de usuários

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', (req, res) => {
     res.status(200).send();
 });
 
+router.get('/users', (req, res) => {
+    const users = [
+        {
+            name: "Jonh Doe",
+            mail: "jonh@mail.com"
+        }
+    ]
+
+    res.json(200, users);
+
+})
+
 module.exports = router;
