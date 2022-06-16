51180a5f933e4879486aa3d54b11150b9553613a
Tiago de Castro Lima <casmei@protonmail.com>
2022-06-16 13:28:14 -0300

    test: deve criar um novo usuário

--- test/user.test.js
+++ test/user.test.js
@@ -1,12 +1,22 @@
-const request = require('supertest');
+const request = require("supertest");
 
-const app = require('../app');
+const app = require("../app");
 
-test('Deve listar todos os usuários', async () => {
-    const res = await request(app).get('/users');
+test("Deve listar todos os usuários", async () => {
+    const res = await request(app).get("/users");
 
     expect(res.status).toBe(200);
     expect(res.body).toHaveLength(1);
-    expect(res.body[0]).toHaveProperty('name', 'Jonh Doe')
+    expect(res.body[0]).toHaveProperty("name", "Jonh Doe");
+
+});
+
+test("Deve criar um novo usuário", async () => {
+    const res = await request(app).post("/users")
+        .send({ name: "Tiago de Castro", email: "a@mail.com" });
+
+    expect(res.status).toBe(201);
+    //expect(res.body.name).toBe("Tiago de Castro Lima");
+
 
 });
