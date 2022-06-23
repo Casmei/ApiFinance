acd3e77820155d4703149275d4dab0f04f0dc552
Tiago de Castro Lima <casmei@protonmail.com>
2022-06-23 07:52:37 -0300

    :sparkles: feat: account controller

--- src/controllers/AccountController.js
+++ src/controllers/AccountController.js
@@ -1,3 +1,14 @@
+const service = require('../service/AccountService')
+
 exports.indexPage = (req, res) => {
     return res.status(200);
+};
+
+exports.createAccount = async (req, res) => {
+    const { name, user_id } = req.body;
+    console.log(`Nome:${name}, User: ${user_id}`)
+    const result = await service.createAccount({ name, user_id });
+    console.log(result)
+    //if (account.error) return res.status(400).json(account.error)
+    return await res.status(201).json(result);
 };
