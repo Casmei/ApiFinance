d3d3e731c434e630d622d4fc9b99103f00a26d53
Tiago de Castro Lima <casmei@protonmail.com>
2022-06-23 17:36:33 -0300

    :sparkles: feat: service layer getAll

--- src/service/AccountService.js
+++ src/service/AccountService.js
@@ -1,6 +1,10 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient()
 
+exports.findAll = async (filter = {}) => {
+    return await prisma.account.findMany
+}
+
 exports.createAccount = async ({ name, user_id }) => {
     return await prisma.account.create({
         data: {
