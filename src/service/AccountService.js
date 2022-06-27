f40f42e796c232a3c808a01cd9b653751dda9455
Tiago de Castro Lima <casmei@protonmail.com>
2022-06-27 09:06:36 -0300

    :sparkles: feat: service layer for findById

--- src/service/AccountService.js
+++ src/service/AccountService.js
@@ -1,8 +1,18 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient()
 
-exports.findAll = async (filter = {}) => {
-    return await prisma.account.findMany
+exports.findAll = () => {
+    const res = prisma.account.findMany();
+    return res
+}
+
+exports.findById = async (filter) => {
+    const res = await prisma.account.findUnique({
+        where: {
+            id: filter.id
+        }
+    })
+    return res
 }
 
 exports.createAccount = async ({ name, user_id }) => {
