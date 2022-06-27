625f78a6db47cab6ad9f50a358918d10f3910914
Tiago de Castro Lima <casmei@protonmail.com>
2022-06-27 16:17:32 -0300

    :sparkles: feat: controller account

--- src/controllers/AccountController.js
+++ src/controllers/AccountController.js
@@ -12,8 +12,18 @@ exports.findById = async (req, res) => {
 };
 
 exports.createAccount = async (req, res) => {
-    const { name, user_id } = req.body;
-    const result = await service.createAccount({ name, user_id });
+    const result = await service.createAccount(req.body);
     //if (account.error) return res.status(400).json(account.error)
     return await res.status(201).json(result);
+};
+
+exports.updateById = async (req, res) => {
+    const result = await service.updateById(req.params, req.body);
+    return await res.status(200).json(result);
+};
+
+exports.deleteById = async (req, res) => {
+    const result = await service.deleteAccount(req.params);
+    return await res.status(204).json(result);
+
 };
