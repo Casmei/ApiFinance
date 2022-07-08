6ec5443273c783a405d5360ab21f7362703ea4a4
Tiago de Castro Lima <casmei@protonmail.com>
2022-07-08 08:55:17 -0300

    Implement validationError in Controller

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,11 +1,15 @@
 const service = require('../service/UserService');
+const validationError = require("../errors/ValidationError");
 const bcrypt = require('bcrypt-nodejs');
 const jwt = require('jwt-simple');
 
 
 exports.singin = async (req, res, next) => {
+
     const user = await service.findOne({ email: req.body.email });
+
     try {
+        if (!user) throw new validationError('Senha ou Usuário inválidos');
         if (bcrypt.compareSync(req.body.password, user.password)) {
             const payload = {
                 id: user.id,
@@ -13,11 +17,11 @@ exports.singin = async (req, res, next) => {
                 email: user.email
             }
             // TODO Criar um Env da Secret Key
-            const token = jwt.encode(payload, '$3dsFs')
-            console.log(token)
-            res.status(200).json({ token })
-        }
-    } catch (err) {
+            const token = jwt.encode(payload, '$3dsFs');
+            res.status(200).json({ token });
+        } else throw new validationError('Senha ou Usuário inválidos');
+    }
+    catch (err) {
         next(err)
     }
 };
