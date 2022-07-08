6cf29e7678107c706f42d24149be908b8ea706f2
Tiago de Castro Lima <casmei@protonmail.com>
2022-07-08 08:54:47 -0300

    🧪 test: user validate

--- test/auth.test.js
+++ test/auth.test.js
@@ -12,4 +12,25 @@ test('Deve receber token ao logar', async () => {
             expect(res.status).toBe(200);
             expect(res.body).toHaveProperty('token');
         })
+});
+
+test('Não deve autenticar usuário com senha errada', () => {
+    const email = `${Date.now()}@auth.com`;
+    return service.createUser(
+        { name: 'Tiago de Castro Lima', email, password: '123456' })
+        .then(() => request(app).post('/auth/singin')
+            .send({ email, password: '123124' }))
+        .then((res) => {
+            expect(res.status).toBe(400);
+            expect(res.body.error).toBe("Senha ou Usuário inválidos");
+        })
+})
+
+test('Não deve autenticar usuário que não existe', async () => {
+    const email = `${Date.now()}@auth.com`;
+
+    const res = await request(app).post('/auth/singin')
+        .send({ email: "naoexiste@fodase.com", password: '123124' });
+    expect(res.status).toBe(400);
+    expect(res.body.error).toBe("Senha ou Usuário inválidos");
 })
