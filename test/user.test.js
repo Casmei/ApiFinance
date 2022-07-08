6d7117e49d3214f193861ec6abc6ec03f84c79e2
Tiago de Castro Lima <casmei@protonmail.com>
2022-07-08 14:05:33 -0300

    refatora os testes  para enviar token ao usuário

--- test/user.test.js
+++ test/user.test.js
@@ -1,22 +1,32 @@
 const request = require("supertest");
+const jwt = require('jwt-simple');
+
 const app = require("../app");
 const service = require('../src/service/UserService');
 
 
-generateCode = (n = 3) => {
-    let text = '';
-    let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    for (let i = 0; i < n; i++) {
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
-    }
-    return text;
-};
+let user;
+let email = Date.now() + "@test.com";
+let password = '123456';
 
-let email = generateCode() + "@test.com";
-let password = generateCode(6);
+beforeAll(async () => {
+    const res = await service.createUser({ name: 'Pedro Gomes', email, password });
+    user = res;
+    user.token = jwt.encode(user, process.env.SECRET_KEY)
+});
+
+test("Não deve inserir usuário sem nome", async () => {
+    const res = await request(app).post('/users')
+        .set('authorization', `bearer ${user.token}`)
+        .send({ email, password })
+
+    expect(res.status).toBe(400);
+    expect(res.body.error).toBe("Nome é um atributo obrigatório");
+});
 
 test("Deve listar todos os usuários", async () => {
-    const res = await request(app).get("/users");
+    const res = await request(app).get("/users")
+        .set('authorization', `bearer ${user.token}`);
 
     expect(res.status).toBe(200);
     expect(res.body.length).toBeGreaterThan(0);
@@ -26,42 +36,40 @@ test("Deve listar todos os usuários", async () => {
 
 test("Deve criar um novo usuário", async () => {
     const res = await request(app).post("/users")
-        .send({ name: "Tiago de Castro Lima", email, password });
+        .send({ name: "Tiago de Castro Lima", email: `${Date.now()} + @algumacoisa.com`, password })
+        .set('authorization', `bearer ${user.token}`);
     expect(res.status).toBe(201);
     expect(res.body).not.toHaveProperty('password');
 });
 
+
 test('Deve armazenar senha criptografada', async () => {
-    let email = generateCode() + "@test.com";
     const res = await request(app).post('/users')
-        .send({ name: 'Pedro Gomes', email, password: 123456 });
+        .set('authorization', `bearer ${user.token}`)
+        .send({ name: 'Pedro Gomes', email: `${Date.now()}@algumacoisa.com`, password: '123456' })
 
     const { id } = res.body;
-    const user = await service.findOne({ id: id });
+    const userFind = await service.findOne({ id: id });
     expect(res.status).toBe(201);
-    expect(user.password).not.toBe(password);
-    expect(user.password).not.toBeUndefined();
-
-
+    expect(userFind.password).not.toBe('123456');
+    expect(userFind.password).not.toBeUndefined();
 })
 
-test("Não deve inserir usuário sem nome", async () => {
-    const res = await request(app).post('/users')
-        .send({ email, password });
-    expect(res.status).toBe(400);
-    expect(res.body.error).toBe("Nome é um atributo obrigatório");
-});
 
 test("Não deve inserir usuário sem email", async () => {
     const res = await request(app).post('/users')
-        .send({ name: "Tiago de Castro Lima", password });
+        .send({ name: "Tiago de Castro Lima", password })
+        .set('authorization', `bearer ${user.token}`);
+
     expect(res.status).toBe(400);
     expect(res.body.error).toBe("Email é um atributo obrigatório");
 });
 
 test("Não deve inserir usuário sem senha", async () => {
     const res = await request(app).post('/users')
-        .send({ name: "Tiago de Castro Lima", email });
+        .send({ name: "Tiago de Castro Lima", email })
+        .set('authorization', `bearer ${user.token}`);
+
     expect(res.status).toBe(400);
     expect(res.body.error).toBe("Senha é um atributo obrigatório");
 });
@@ -69,6 +77,8 @@ test("Não deve inserir usuário sem senha", async () => {
 test("Não deve inserir usuário com email existente", async () => {
     const res = await request(app).post('/users')
         .send({ name: "Tiago de Castro Lima", email, password })
+        .set('authorization', `bearer ${user.token}`);
+
     expect(res.status).toBe(400);
     expect(res.body.error).toBe("Já existe um usuário com este email");
 })
