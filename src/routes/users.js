16f33a6076b02c4869006835abe53f4e682743cd
Tiago de Castro Lima <casmei@protonmail.com>
2022-07-08 14:04:18 -0300

    define o controle de acesso as rotas

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,9 +1,13 @@
-const controllerUser = require('../controllers/UsersController');
 const express = require('express');
+const passport = require('../config/passport');
+
+const controllerUser = require('../controllers/UsersController');
+
 
 const router = express.Router();
 
 router.route('/')
+    .all(passport.authenticate())
     .get(controllerUser.allUsers)
     .post(controllerUser.createUser);
 
